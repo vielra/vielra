aeaf4df890df7499b4d3a01e64fbd594afce6670
hiriski <hiriski@outlook.com>
2023-11-25 16:53:54 +0700

    fix: conflicts persist config

--- src/plugins/redux/config-store.ts
+++ src/plugins/redux/config-store.ts
@@ -30,16 +30,14 @@ import { persistStorage } from './persist-storage';
 // reducers
 import { appApi } from '@/modules/app/redux';
 import { baseApi } from '@/plugins/redux/base.api';
-import { authApi, authSlice } from '@/modules/auth/redux';
 import { phrasebookApi, phrasebookPersistedSlice } from '@/modules/phrasebook/redux';
 import { authApi, persistedAuthSlice } from '@/modules/auth/redux';
-import { phrasebookApi, phrasebookSlice } from '@/modules/phrasebook/redux';
 
 // persist config
 const persistConfig: PersistConfig<RootState> = {
   key: '@ROOT',
   storage: persistStorage,
-  whitelist: [persistedAuthSlice.name, phrasebookSlice.name, phrasebookPersistedSlice.name],
+  whitelist: [persistedAuthSlice.name, phrasebookPersistedSlice.name],
 };
 
 // make persisted store
